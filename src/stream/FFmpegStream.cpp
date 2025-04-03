bfe84b7857699efbd5a08db5255306609b2b0988
Matt Filetto <matt.filetto@gmail.com>
2025-04-02 19:18:09 -0700

    der
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/stream/FFmpegStream.cpp
+++ src/stream/FFmpegStream.cpp
@@ -900,7 +900,7 @@ bool FFmpegStream::OpenWithFFmpeg(const AVInputFormat* iformat, const AVIOInterr
   av_dict_set(&options, "read_ahead_limit", "-1", 0);
 
   av_dict_set(&options, "cache", "1", 0);
-  av_dicdddddddt_set(&options, "reconnect", "1", 0);
+  av_dict_set(&options, "reconnect", "1", 0);
   av_dict_set(&options, "reconnect_streamed", "1", 0);
   av_dict_set(&options, "reconnect_on_network_error", "1", 0);
   av_dict_set(&options, "fflags", "+genpts", 0); // Ensure PTS generation if missing
